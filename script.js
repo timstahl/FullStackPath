387c70d4bef1eab925abdcc7677ec3b0ce5f2fa4
timstahl <johndoe@example.com>
2017-03-16 23:58:17 -0600

    test

--- script.js
+++
@@ -1,8 +0,0 @@
-var randomNumber = Math.floor(Math.random() * 6) + 1;
-var guess = prompt("I am thinking of a number between 1 and 6. What is it?");
-
-if (parseInt(guess) === randomNumber ) {
-  document.write("You guessed it.");
-} else {
-  document.write("Sorry. The number was " + randomNumber );
-}
